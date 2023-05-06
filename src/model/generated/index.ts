c40641c8a9edbb9efa3ad1e9dfc2d5e4972a08d3
Tanay Patil <tanaypatil2003@gmail.com>
2023-05-06 16:58:16 -0700

    add owners and tokens

--- src/model/generated/index.ts
+++ src/model/generated/index.ts
@@ -1,1 +1,4 @@
 export * from "./burn.model"
+export * from "./owner.model"
+export * from "./token.model"
+export * from "./transfer.model"
--- src/processor.ts
+++ src/processor.ts
@@ -1,12 +1,27 @@
-import { TypeormDatabase } from '@subsquid/typeorm-store';
-import {EvmBatchProcessor} from '@subsquid/evm-processor'
-import { lookupArchive } from '@subsquid/archive-registry'
-import assert from 'assert';
-import { Burn } from './model';
+import { TypeormDatabase, Store } from '@subsquid/typeorm-store';
+import { EvmBatchProcessor, BatchProcessorItem, BatchHandlerContext } from '@subsquid/evm-processor';
+import { lookupArchive } from '@subsquid/archive-registry';
+
+import { Owner, Token, Transfer } from './model';
 
 import * as nouns from './abi/nouns';
+import { BigNumber } from 'ethers';
 
-const CONTRACT_ADDRESS = '0x9c8ff314c9bc7f6e59a9d9225fb22946427edc03';
+const NOUNS_ADDRESS = '0x9c8ff314c9bc7f6e59a9d9225fb22946427edc03';
+const EXHIBIT_BASE_ADDRESS = '0x9c8ff314c9bc7f6e59a9d9225fb20946427edc03';
+
+type Item = BatchProcessorItem<typeof processor>;
+type Context = BatchHandlerContext<Store, Item>;
+
+interface RawTransfer {
+  id: string;
+  tokenId: number;
+  from: string;
+  to: string;
+  timestamp: Date;
+  blockNumber: number;
+  txHash: string;
+}
 
 const processor = new EvmBatchProcessor()
   .setDataSource({
@@ -20,11 +35,12 @@ const processor = new EvmBatchProcessor()
     // see https://docs.subsquid.io/develop-a-squid/evm-processor/configuration/
 
     archive: lookupArchive('eth-mainnet'),
+    chain: 'https://rpc.ankr.com/eth',
   })
   .setBlockRange({
     from: 12_985_438,
   })
-  .addLog(CONTRACT_ADDRESS, {
+  .addLog(NOUNS_ADDRESS, {
     filter: [[nouns.events.Transfer.topic]],
     data: {
       evmLog: {
@@ -36,21 +52,21 @@ const processor = new EvmBatchProcessor()
       },
     },
   })
-  .addTransaction(['0x0000000000000000000000000000000000000000'], {
-    range: {
-      from: 6_000_000,
-    },
+  .addLog(EXHIBIT_BASE_ADDRESS, {
+    filter: [[nouns.events.Transfer.topic]],
     data: {
+      evmLog: {
+        topics: true,
+        data: true,
+      },
       transaction: {
-        from: true,
-        value: true,
         hash: true,
       },
     },
   });
 
-function formatID(height:any, hash:string) {
-  return `${String(height).padStart(10, '0')}-${hash}`
+function formatID(height: any, hash: string) {
+  return `${String(height).padStart(10, '0')}-${hash}`;
 }
 
 // processor.run(new TypeormDatabase(), async (ctx) => {
@@ -79,12 +95,86 @@ function formatID(height:any, hash:string) {
 // });
 
 processor.run(new TypeormDatabase(), async (ctx) => {
+  let rawTransfers: RawTransfer[] = getRawTransfers(ctx);
+
+  let owners: Map<string, Owner> = createOwners(rawTransfers);
+  let tokens: Map<string, Token> = createTokens(rawTransfers, owners);
+  let transfers: Transfer[] = createTransfers(rawTransfers, owners, tokens);
+
+  let lastBatchedBlockHeader = ctx.blocks[ctx.blocks.length - 1].header;
+  let contract = new nouns.Contract(ctx, lastBatchedBlockHeader, NOUNS_ADDRESS);
+  // for (let t of tokens.values()) {
+  //   const uri = await contract.tokenURI(BigNumber.from(t.tokenId));
+  //   ctx.log.info(`Token ${t.tokenId} has metadata: ${uri}`);
+  // }
+
+  await ctx.store.upsert([...owners.values()]);
+  await ctx.store.upsert([...tokens.values()]);
+  await ctx.store.insert(transfers);
+});
+
+function getRawTransfers(ctx: Context): RawTransfer[] {
+  let transfers: RawTransfer[] = [];
+
   for (let block of ctx.blocks) {
     for (let item of block.items) {
       if (item.kind !== 'evmLog') continue;
       let { from, to, tokenId } = nouns.events.Transfer.decode(item.evmLog);
-      ctx.log.info(`Transfer ${from} -> ${to} tokenId: ${tokenId}`);
+      transfers.push({
+        id: item.evmLog.id,
+        tokenId: tokenId.toNumber(),
+        from,
+        to,
+        timestamp: new Date(block.header.timestamp),
+        blockNumber: block.header.height,
+        txHash: item.transaction.hash,
+      });
     }
   }
-})
 
+  return transfers;
+}
+
+function createOwners(rawTransfers: RawTransfer[]): Map<string, Owner> {
+  let owners: Map<string, Owner> = new Map();
+  for (let t of rawTransfers) {
+    owners.set(t.from, new Owner({ id: t.from }));
+    owners.set(t.to, new Owner({ id: t.to }));
+  }
+  return owners;
+}
+
+function createTokens(rawTransfers: RawTransfer[], owners: Map<string, Owner>): Map<string, Token> {
+  let tokens: Map<string, Token> = new Map();
+  for (let t of rawTransfers) {
+    let tokenIdString = `${t.tokenId}`;
+    tokens.set(
+      tokenIdString,
+      new Token({
+        id: tokenIdString,
+        tokenId: t.tokenId,
+        owner: owners.get(t.to),
+      })
+    );
+  }
+  return tokens;
+}
+
+function createTransfers(
+  rawTransfers: RawTransfer[],
+  owners: Map<string, Owner>,
+  tokens: Map<string, Token>
+): Transfer[] {
+  return rawTransfers.map(
+    (t) =>
+      new Transfer({
+        id: t.id,
+        token: tokens.get(`${t.tokenId}`),
+        from: owners.get(t.from),
+        to: owners.get(t.to),
+        timestamp: t.timestamp,
+        blockNumber: t.blockNumber,
+        txHash: t.txHash,
+      })
+  );
+}
